0c65729a27f578c0c375e9e631257143e85bf6e2
jmargolisvt <jeff@jmargolisvt.com>
2016-10-26 18:24:52 -0400

    save sorted setlists

--- public/js/dnd.js
+++ public/js/dnd.js
@@ -16,10 +16,29 @@ $(document).ready(function() {
               // because JQuery UI adds inline styles
               position: 'relative',
               left: '0px',
-              top: '0px',
-              width: 'inherit',
-              height: 'inherit'
+              top: '0px'
         }));
+        getOrder();
     }
   });
 });
+
+// get all the song id's and put them in an object
+function getOrder() {
+  var x = document.querySelectorAll('#the_bench .song');
+  var y = document.querySelectorAll('#set_1 .song');
+  var z = document.querySelectorAll('#set_2 .song');
+  var list = new Object;
+  build_list('bench', x, list);
+  build_list('set_1', y, list);
+  build_list('set_2', z, list);
+  //return (JSON.stringify(list));
+  document.getElementById("songArray").value = JSON.stringify(list);
+}
+
+function build_list(cat, songs, list) {
+  list[cat] = new Array;
+  for (i=0; i< songs.length; i++) {
+    list[cat].push({[songs[i].getAttribute('data-id')]: songs[i].getAttribute('data-name')});
+  }
+}
