3ee4bea28debdb7b7031cc03d7f84b3e65d5fe7e
jmargolisvt <jeff@jmargolisvt.com>
2017-03-19 15:21:03 -0400

    refactor song counting

--- public/js/dnd.js
+++ public/js/dnd.js
@@ -26,17 +26,19 @@ $(document).ready(function() {
 });
 var list = new Object;
 
-// get all the song id's and put them in an object
-// exclude JQuery UI's placeholder li's
 function getOrder() {
   let sections = ['bench', 'set_1', 'set_2', 'set_3'];
   for (var i = 0; i < sections.length; i++) {
     buildList(sections[i], document.querySelectorAll('#'+ sections[i] + ' .song:not(.ui-sortable-placeholder)'));
+    // exclude JQuery UI's placeholder li's
+    var elem = document.getElementById('count_' + i);
+    if(elem) {
+      elem.innerHTML = list[sections[i]].length;
+    }
   }
 
   document.getElementById("songArray").value = JSON.stringify(list);
 
-  //updateCount(set1, set2);
 }
 
 function buildList(cat, songs) {
@@ -44,9 +46,4 @@ function buildList(cat, songs) {
   for (i=0; i< songs.length; i++) {
     list[cat].push({[songs[i].getAttribute('data-id')]: songs[i].getAttribute('data-name')});
   }
-}
-
-function updateCount(set1, set2) {
-  document.getElementById('count_1').innerHTML = set1.length - document.querySelectorAll('#set_1 li.ui-sortable-placeholder').length;
-  document.getElementById('count_2').innerHTML = set2.length - document.querySelectorAll('#set_2 li.ui-sortable-placeholder').length;
 }
