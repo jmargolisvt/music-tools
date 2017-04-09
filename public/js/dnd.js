4dc8964d207a9cfb2135975374e380e2acb1b8f0
jmargolisvt <jeff@jmargolisvt.com>
2017-04-08 22:31:08 -0400

    add up and display total set times

--- public/js/dnd.js
+++ public/js/dnd.js
@@ -34,12 +34,16 @@ function getOrder() {
   let sections = ['bench', 'set_1', 'set_2', 'set_3'];
   for (var i = 0; i < sections.length; i++) {
     buildList(sections[i], document.querySelectorAll('#'+ sections[i] + ' .song:not(.ui-sortable-placeholder)'));
+
     // exclude JQuery UI's placeholder li's
     var elem = document.getElementById('count_' + i);
+    // update the count
     if(elem) {
       elem.innerHTML = list[sections[i]].length;
     }
   }
+  // update the set time
+  updateTime(list);
   document.getElementById("songArray").value = JSON.stringify(list);
 }
 
@@ -50,4 +54,27 @@ function buildList(cat, songs) {
       {'name':songs[i].getAttribute('data-name'),
        'time':songs[i].getAttribute('data-time')}});
   }
+}
+
+function updateTime(list) {
+  for (set in list) {
+    // build arrays
+    let x = list[set],
+        total = [];
+    for (i=0; i<x.length; i++) {
+      let time = x[i][Object.keys(x[i])[0]]['time'];
+      if ( time !== 'undefined') {
+        total.push(time);
+      }
+    }
+
+    // add up time and display it
+    let arr = [],
+        min = 0;
+    total.forEach((x) => arr.push(parseInt(x)));
+    if (arr.length > 0 && set !== 'bench') {
+      min = arr.reduce( (a,b) => a + b );
+      document.getElementById(set).querySelector('.total-time').innerHTML = min;
+    }
+  };
 }
